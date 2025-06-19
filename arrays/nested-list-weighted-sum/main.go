e0ec1816ac1be49cf3294acd92a21fe739bf103c
yassine benlamkadem <yassine.benlamkadem@gmail.com>
2025-06-19 23:12:33 +0100

    nested-list-weighted-sum

---
+++ arrays/nested-list-weighted-sum/main.go
@@ -0,0 +1,21 @@
+package main
+
+func NestedListWeightedSum(arr []interface{}, sum, depth int) int {
+	for _, e := range arr {
+		if IsInt(e) {
+			sum += e.(int) * depth
+		} else {
+			sum += NestedListWeightedSum(e.([]interface{}), sum, depth+1)
+		}
+	}
+	return sum
+}
+
+// The list can contain both integers and slices of integers so the best we can do in go is []interface
+func IsInt(e interface{}) bool {
+	switch e.(type) {
+	case int:
+		return true
+	}
+	return false
+}
