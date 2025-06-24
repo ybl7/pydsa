ca2934f5e5dd6a1341762e7c900a7e84771a3559
yassine benlamkadem <yassine.benlamkadem@gmail.com>
2025-06-24 23:10:35 +0100

    pow-by-squaring

---
+++ arrays/pow/main.go
@@ -0,0 +1,25 @@
+package main
+
+func Pow(x float64, n int) float64 {
+	// Inner function to perform the quick exponentiation.
+	quickPower := func(base float64, exp int) float64 {
+		result := 1.0
+		for exp != 0 {
+			// Odd exponent, store base^1 in result, we will lose it later in exp >>= 1
+			// Only encountered twice, at the start if the exponent is odd, at the end when we reach exponent 1
+			if exp&1 != 0 {
+				result *= base
+			}
+			// Exp is at least two, so we can square the base then halve it
+			base *= base
+			exp >>= 1
+		}
+		return result
+	}
+
+	if n >= 0 {
+		return quickPower(x, n)
+	} else {
+		return 1 / quickPower(x, -n)
+	}
+}
